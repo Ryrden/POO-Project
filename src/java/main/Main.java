6ff09945153c5e058cc9c7a217ddcf7813f5ec38
Ryan Souza Sá Teles <ryan.teles@opus-software.com.br>
2022-08-04 11:56:46 -0300

    build: start at phase 1 now

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
             Screen screen = new Screen();
-            screen.loadPhase(3);
+            screen.loadPhase(1);
             screen.setVisible(true);
             screen.createBufferStrategy(2);
             screen.go();
